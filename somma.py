a124334aa382fd9802be8d5d025ad864714e5ea7
SilentSea <m.serafini@protonmail.com>
2025-03-18 13:00:41 +0100

    Update somma.py

--- somma.py
+++ somma.py
@@ -1,4 +1,4 @@
-valoer1 = input("Inserisci il primo valore: ")
+valore1 = input("Inserisci il primo valore: ")
 valore2 = input("Inserisci il secondo valore: ")
 v1 = int(valore1)
 v2 = int(valore2)
