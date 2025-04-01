90a91c7008f27f21749768c456ce093729a942bb
SilentSea <m.serafini@protonmail.com>
2025-04-01 13:06:51 +0200

    Update imposta.py

--- imposta.py
+++ imposta.py
@@ -4,10 +4,10 @@ reddito = float(reddito)
 if reddito <= 28000:
     imposta = (reddito / 100) * 23
     print("Imposta da pagare al 23%:", imposta)
-elif reddito >= 29000 and reddito <= 50000:
+elif reddito > 28000 and reddito <= 50000:
     imposta = (reddito / 100) * 35
     print("Imposta da pagare al 35%:", imposta)
-elif reddito >= 51000 and reddito <= 75000: 
+elif reddito > 50000 and reddito <= 75000: 
     imposta = (reddito / 100) * 43
     print("Imposta da pagare al 43%:", imposta)
 else:  
