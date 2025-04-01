0bb13cc2645b0f60bd7be23156a4f600921f527f
SilentSea <m.serafini@protonmail.com>
2025-04-01 10:26:29 +0200

    Update carte_da_poker.py

--- carte_da_poker.py
+++ carte_da_poker.py
@@ -5,6 +5,6 @@ quadri = "\u2663"
 cuori = "\u2661"
 print(cuori, quadri, fiori, picche) 
 
-smei_bytes = (b'\xe2\x99\xa0' + b'\xe2\x99\xa5' + b'\xe2\x99\xa6' + b'\xe2\x99\xa3')
-str = smei_bytes.decode()
+semi_bytes = (b'\xe2\x99\xa0' + b'\xe2\x99\xa5' + b'\xe2\x99\xa6' + b'\xe2\x99\xa3')
+str = semi_bytes.decode()
 print(str)
