b1ae687486464d6794b6c0848b054ccc13a0eeaa
SilentSea <m.serafini@protonmail.com>
2025-04-01 10:25:48 +0200

    Update carte_da_poker.py

--- carte_da_poker.py
+++ carte_da_poker.py
@@ -5,4 +5,6 @@ quadri = "\u2663"
 cuori = "\u2661"
 print(cuori, quadri, fiori, picche) 
 
-
+smei_bytes = (b'\xe2\x99\xa0' + b'\xe2\x99\xa5' + b'\xe2\x99\xa6' + b'\xe2\x99\xa3')
+str = smei_bytes.decode()
+print(str)
