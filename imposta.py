6457630ef45df707ede6175ba33cac1d80a4067d
SilentSea <m.serafini@protonmail.com>
2025-04-01 12:24:43 +0200

    Add files via upload

---
+++ imposta.py
@@ -0,0 +1,12 @@
+reddito = input("Inserisci il tuo reddito: ")
+reddito = int(reddito)
+
+if reddito <= 28000:
+    imposta = (reddito / 100) * 23
+    print("Imposta da pagare al 23%:", imposta)
+elif reddito <= 50000:
+    imposta = (reddito / 100) * 35
+    print("Imposta da pagare al 35%:", imposta)
+elif reddito > 75000:
+    imposta = (reddito / 100) * 43
+    print("Imposta da pagare: al 43%", imposta)
