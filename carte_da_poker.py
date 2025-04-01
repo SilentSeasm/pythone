104740c0711cb53e8bec77119741ad3e9b2575d5
SilentSea <m.serafini@protonmail.com>
2025-04-01 10:34:28 +0200

    Update carte_da_poker.py

--- carte_da_poker.py
+++ carte_da_poker.py
@@ -1,10 +1,11 @@
-
+#unicode
 fiori = "\u2662"
 picche = "\u2664" 
 quadri = "\u2663"
 cuori = "\u2661"
 print(cuori, quadri, fiori, picche) 
 
+#utf
 semi_bytes = (b'\xe2\x99\xa0' + b'\xe2\x99\xa5' + b'\xe2\x99\xa6' + b'\xe2\x99\xa3')
 str = semi_bytes.decode()
 print(str)
