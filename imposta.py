3e0f1b4c8be73926ddac9484e07395de98d969d8
SilentSea <m.serafini@protonmail.com>
2025-04-01 12:29:52 +0200

    Update imposta.py

--- imposta.py
+++ imposta.py
@@ -7,6 +7,9 @@ if reddito <= 28000:
 elif reddito <= 50000:
     imposta = (reddito / 100) * 35
     print("Imposta da pagare al 35%:", imposta)
-elif reddito > 75000:
+elif reddito <= 75000: 
     imposta = (reddito / 100) * 43
-    print("Imposta da pagare: al 43%", imposta)
+    print("Imposta da pagare al 43%:", imposta)
+else:  
+    imposta = (reddito / 100) * 43  
+    print("Imposta da pagare al 43%:", imposta)
