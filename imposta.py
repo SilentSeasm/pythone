ae8014eb726773d0ee92dc54e4eed8e3ec535c2e
SilentSea <m.serafini@protonmail.com>
2025-04-01 12:39:08 +0200

    Update imposta.py

--- imposta.py
+++ imposta.py
@@ -1,9 +1,6 @@
 reddito = input("Inserisci il tuo reddito: ")
 reddito = int(reddito)
 
-#if type(reddito) != int:
-#   print("Il reddito deve essere un numero intero. Riprova")
-
 if reddito <= 28000:
     imposta = (reddito / 100) * 23
     print("Imposta da pagare al 23%:", imposta)
