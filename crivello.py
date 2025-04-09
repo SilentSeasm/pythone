53cd4266a523950b7836bbfbef5d04822b9faec9
SilentSea <m.serafini@protonmail.com>
2025-04-09 12:35:10 +0200

    Add files via upload

---
+++ crivello.py
@@ -0,0 +1,18 @@
+numero = int(input("Inserisci un numero: "))
+
+def calcolo_numeri_primi(numero):
+    numeri_primi = []
+    for num in range(2, numero + 1):  
+        is_primo = True
+        for divisore in range(2, int(num ** 0.5) + 1):  
+            if num % divisore == 0:
+                is_primo = False
+                break
+        if is_primo:
+            numeri_primi.append(num)
+    return numeri_primi
+
+primi = calcolo_numeri_primi(numero)
+conta = len(primi)
+print(conta, "numeri primi: ")
+print(primi)
