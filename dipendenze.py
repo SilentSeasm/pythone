c6a6d49d4929ced37507e7af4fb65c11fc26a2a8
SilentSea <m.serafini@protonmail.com>
2025-04-29 12:52:21 +0200

    Update dipendenze.py

--- dipendenze.py
+++ dipendenze.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://lipsum.com'  
+url = 'https://lipsum.com'
 
 risposta = requests.get(url)
 
-soup = BeautifulSoup(risposta.text, 'html.parser')
+parsed = BeautifulSoup(risposta.text, 'html.parser')
 
-link_tags = soup.find_all('a')
+link_tags = parsed.find_all('a')
 
-if risposta.status_code==200:
+if risposta.status_code == 200:
     print("Link trovati nella pagina:")
     for tag in link_tags:
-        print(tag['href'])
+        if 'href' in tag.attrs: 
+            print(tag['href'])
 else:
     print("Errore nella richiesta:", risposta.status_code)
-
